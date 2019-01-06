f67c38becffd2bc13214a2b647b38f039c90543e
evnpvn <evan@evanpavan.com>
2019-01-06 17:38:43 -0500

    First commit after creating new program for Treehouse Streams and data processing

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SocccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var files = directory.GetFiles("*.txt");
+            foreach(var file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+        }
+    }
+}
