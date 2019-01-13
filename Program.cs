7e28b4dd37f3c5475a1d6436a05c86248feaaa14
evan <evan@evanpavan.com>
2019-01-13 12:20:13 -0500

    Changes made after completing Reading Data section

--- Program.cs
+++ Program.cs
@@ -7,42 +7,34 @@ using System.IO;
 
 namespace SoccerStats
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //Get the current folder name that we are in and assign it to a string variable
             string currentDirectory = Directory.GetCurrentDirectory();
 
-            //Initializes new DirectoryInfo object and pass it the currentDirectory name as a parameter for construction
-            //We did this so that we could use DirectoryInfo's property of FullName below
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 
-            Console.WriteLine(directory.FullName);
+            string fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
 
-            //created a string variable for the filename. Path.Combine method combines a number of strings into a Filepath
-            //We're using this method to append \data.txt to the directory. We could have done it with just a string but this is a bestpractice
-            string fileName = Path.Combine(directory.FullName, "data.txt");
+            string fileContents = ReadFile(fileName);
 
-            //initializing a new instance of FileInfo object against our fully qualified fileName
-            //allows us to use the exists property in the if statement to check if the file exists before using it.
-            FileInfo file = new FileInfo(fileName);
+            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
-            Console.WriteLine(fileName);
-            Console.WriteLine(file.FullName);
-
-            if(file.Exists)
+            foreach(string line in fileLines)
             {
-                //Created a new StreamReader instance and passed it the File's FullName.
-                //Changed the Console's default stream to read from the file.
-                //Read the file's text to the Console, and passed that to the Console to write to the console.
-                //wrapped it all in a using block because we want to remove the file from memory after this block since we are done with it.
-                using (StreamReader reader = new StreamReader(file.FullName))
-                {
-                    Console.SetIn(reader);
-                    Console.WriteLine(Console.ReadLine());
-                }
+                Console.WriteLine(line);
             }
         }
+
+        public static string ReadFile(string fileName)
+        {   
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+
+        }   
+    
     }
 }
